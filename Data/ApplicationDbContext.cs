6ef00b87de6b5810c51925f0b4ffc1f721ea51af
Mutayyab Imran <mutayyab786yo@gmail.com>
2024-07-26 15:40:25 +0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HelpDeskSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace HelpDeskSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
