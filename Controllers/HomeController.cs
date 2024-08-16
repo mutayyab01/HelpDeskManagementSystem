8404a1f9e5f68af6aee345c02af24e54fec886bf
Mutayyab Imran <mutayyab786yo@gmail.com>
2024-08-16 17:22:21 +0500

    Added Admin Template

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
+using HelpDeskSystem.Data;
 using HelpDeskSystem.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace HelpDeskSystem.Controllers
@@ -7,15 +9,24 @@ namespace HelpDeskSystem.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _context;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
+
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var tickets = await _context.Tickets
+                           .Include(t => t.CreatedBy)
+                           .OrderBy(x => x.CreatedOn)
+                           .ToListAsync();
+
+
+
+            return View(tickets);
         }
 
         public IActionResult Privacy()
